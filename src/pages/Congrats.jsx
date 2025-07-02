4cca9173e4faebb83d62d4d2be9cc3e155d4de02
prince-singh19 <princesingh9621680755@gmail.com>
2025-07-02 17:15:38 +0530

    add election page

--- src/pages/Congrats.jsx
+++ src/pages/Congrats.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Congrats = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="congrats">
+      <div className="container congrats__container">
+        <h2>Thanks for your vote!</h2>
+        <p>
+          Your vote is now added to your candidate's vote count. You will be redirected shortly to see the new result.
+        </p>
+        <Link to="/results" className="btn sm primary">See Results</Link>
+      </div>
+    </section>
   )
 }
 
